e7e93f66322e6195dbfbc718e1fccc9dc33760a5
Graham TerMarsch <graham@howlingfrog.com>
2010-01-13 14:25:04 -0800

    Version bump.

--- lib/TAP/Formatter/JUnit.pm
+++ lib/TAP/Formatter/JUnit.pm
@@ -7,7 +7,7 @@ use TAP::Formatter::JUnit::Session;
 use base qw(TAP::Formatter::Console);
 use Class::Field qw(field);
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 field 'testsuites'  => [];
 
